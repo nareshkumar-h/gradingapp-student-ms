43fb9882c719527382eca728099aa346c96e74ac
CHANDRA BABU BOLLINENI <chandrababubollineni416@gmail.com>
2019-10-25 13:48:34 +0530

    created project and added application.properties

---
+++ src/main/java/com/revature/grademanagementsystemstudentms/GrademanagementsystemStudentMsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.grademanagementsystemstudentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrademanagementsystemStudentMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrademanagementsystemStudentMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/grademanagementsystemstudentms/GrademanagementsystemStudentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.grademanagementsystemstudentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrademanagementsystemStudentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
